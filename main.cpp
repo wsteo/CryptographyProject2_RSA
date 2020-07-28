5b8d31240c914984471af1d03b912a7f2ed9cab7
calvin7125 <67590726+calvin7125@users.noreply.github.com>
2020-07-28 22:19:47 +0800

    FileProcessingNumberOnly

--- main.cpp
+++ main.cpp
@@ -1,184 +1,138 @@
 #include <iostream>
-#include<cmath>
-#include<cstdlib>
-#include<cstring>
-#include<conio.h>
+#include <fstream>
 
 using namespace std;
 
-bool isCoPrime(long int e,long int phi){
-    long int lowest;
-    long int i=2;
-    bool coprime=true;
-
-    if (phi>e){
-        lowest=e;
-    }else{
-        lowest=phi;
-    }
-
-    while(i<lowest){
-        if(!(phi%i) && !(e%i)){
-            coprime=false;
-        }
-        i++;
-    }
-    return coprime;
-}
-
-bool isPrime(long int n){
-    if (n<2) return false;
+bool isPrime (long int n) {
+    if (n < 2) return false;
 
     long int i = 2;
 
-    while(i<=n/2){
-        if(!(n%i)) return false;
+    while (i < n / 2) {
+        if (n % i == 0) return false;
+        i++;
+    }
+
+    return true;
+}
+
+bool isCoPrime (long int e, long int phi_n) {
+    long int i = 2;
+    while (i < e) {
+        if ((e % i == 0) && (phi_n % i == 0)) return false;
         i++;
     }
     return true;
 }
 
-long int findE(long int phi){
-    long int e=0;
-    do{
-        cout << "Enter e value: ";
+long int findE (long int phi_n) {
+    long int e;
+    do {
+        cout << "Put e value: ";
         cin >> e;
-    }while (!isCoPrime (e,phi));
+    } while (!isCoPrime (e, phi_n));
     return e;
 }
 
-long int findD(long int e, long int phi){
-    long int a=phi,b=e;
-    long int x=0,y=1,u=1,v=0,m,n,q,r;
-    long int gcd=b;
+long int findD (long int e, long int phi_n) {
+    long int a =phi_n, b = e;
+    long int x = 0, y = 1, u = 1, v = 0, m, n, q, r;
+    long int gcd = b;
 
-    while(a!=0){
-        q=gcd/a;
-        r=gcd%a;
-        m=x-u*q;
-        n=y-v*q;
-        gcd=a;
-        a=r;
-        x=u;
-        y=v;
-        u=m;
-        v=n;
+    while (a != 0) {
+        q = gcd / a;
+        r = gcd % a;
+        m = x - u*q;
+        n = y - v*q;
+        gcd = a;
+        a = r;
+        x = u;
+        y = v;
+        u = m;
+        v = n;
     }
-    if(y<1){
-        y=phi+y;
+    if (y < 1) {
+        return phi_n + y;
     }
+
     return y;
 }
 
-void KeyGen(long int &e, long int &d, long int &n){
-    long int p,q,phi; //prime numbers
+void GenerateKey (long int &e, long int &d, long int &n) {
+    long int p, q;
 
-    do{
-        cout << "Enter Prime Number(p): ";
+    do {
+        cout << "Put a prime number: ";
         cin >> p;
-    }while (!isPrime(p));
+    } while (!isPrime(p));
 
-    do{
-        cout << "Enter Second Prime Number(q): ";
+    do {
+        cout << "Put a prime number: ";
         cin >> q;
-    }while (!isPrime(q));
+    } while (!isPrime(q));
 
-    n=p*q;
-    cout << "n is " << n << endl;
+    n = p * q;
+    long int phi_n = (p-1) * (q-1);
 
-    phi=(p-1)*(q-1);
-    cout << "phi is " << phi << endl;
+    e = findE (phi_n);
+    d = findD (e, phi_n);
 
-    e=findE(phi);
-    cout << "e is " << e << endl;
-    if(!e){
-        cout << "Choose two suitable prime number" << endl;
-        exit(1);
-    }
-
-    d=findD(e,phi);
-    cout << "d is " << d << endl;
+    return;
 }
 
-long int EncryptDecrypt(long int t, long int e_d, long int n){
-    long int remainder;
-    long int x=1;
+long int EncryptDecrypt (long int t, long int EorD, long int n) {
+    long int rem;
+    long int x = 1;
 
-    while(e_d != 0){
-        remainder = e_d%2;
-        e_d=e_d/2;
+    while (EorD != 0) {
+        rem = EorD % 2;
+        EorD = EorD / 2;
 
-        if (remainder==1){
-            x=(x*t)%n;
+        if (rem == 1) {
+            x = (x * t) % n;
         }
-        t=(t*t)%n; //increment
+
+        t = (t * t) % n;
     }
+
     return x;
-}
-
-void EncryptDecryptString(long int e, long int n)
-{
-    char *str=new char[1000];
-    char *str1=new char[1000];
-
-    cout << "\nEnter a string: ";
-    cin >> str;
-
-    cout << "Encrypting using Public Key: " << endl;
-    int i=0;
-    while(i!=strlen(str)){
-        str1[i]=EncryptDecrypt(str[i],e,n);
-        i++;
-    }
-    cout << str1 << endl;
-}
-
-void EncryptDecryptNumber(long int num1, long int num2)
-{
-    long int n;
-
-    cout << "\nEnter an integer number: ";
-    cin >> n;
-
-    cout << EncryptDecrypt(n,num1,num2) << endl;
 }
 
 int main()
 {
-    long int e,d=0,n;
-    KeyGen(e,d,n);
-    cout << "public key: ("<< e << ","<< n << ")" << endl;
-    cout << "private key: ("<< d << ","<< n << ")" << endl;
+    long int e, d, n;
+    GenerateKey(e, d, n);
+    cout << "Public Key: (" << e << "," << n << ")" << endl;
+    cout << "Private Key: (" << d << "," << n << ")" << endl;
 
-    cout << "\nPress 1: Encrypt Numbers" << "\nPress 2: Encrypt String" <<endl;
-    int choice;
-    cin >> choice;
+    fstream file;
+    int content;
+    file.open("PlainText.txt");
+    file>>content;
+    cout << "From PlainText File: " << content <<endl;
+    file.close();
 
-    switch (choice){
-        case 1:
-            EncryptDecryptNumber(e,n);
-            break;
-        case 2:
-            EncryptDecryptString(e,n);
-            break;
-        default:
-            cout<<"Invalid option"<<endl;
-            exit(1);
-    }
+    long int C_ = EncryptDecrypt(content, e, n);
+    long int C = EncryptDecrypt(C_, d, n);
 
-    cout << "\nPress 1: Decrypt Numbers" << "\nPress 2: Decrypt String" <<endl;
+    file.open("EncryptedText.txt");
+    file<<C;
+    file.close();
 
-    switch (choice){
-        case 1:
-            EncryptDecryptNumber(d,n);
-            break;
-        case 2:
-            EncryptDecryptString(d,n);
-            break;
-        default:
-            cout<<"Invalid Option"<<endl;
-            exit(1);
-    }
-    getch();
+    int content2;
+    file.open("EncryptedText.txt");
+    file>>content2;
+    cout << "From EncryptexText File: " << content2 <<endl;
+    file.close();
+
+    long int M_ = EncryptDecrypt(content2, d, n);
+    long int M = EncryptDecrypt(M_, e, n);
+
+    file.open("DecryptedText.txt");
+    file<<M;
+    file.close();
+
+    cout << "Ciphertext: " << C << " Plaintext: " << M << endl;
+
     return 0;
 }
